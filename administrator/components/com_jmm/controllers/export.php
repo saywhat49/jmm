03057f0b481e9336daad0ce8fc8669fe0624ba32
Biswarup Adhikari <biswarupadhikari@gmail.com>
2013-01-16 21:06:48 +0530

    Custom Query Result Can be Exported as CSV File

---
+++ administrator/components/com_jmm/controllers/export.php
@@ -0,0 +1,33 @@
+<?php
+defined('_JEXEC') or die('Restricted access');
+jimport('joomla.application.component.controlleradmin');
+class JMMControllerExport extends JControllerAdmin
+{
+	/**
+	 * Export In CSV
+	 */
+	function csv(){
+		$mainframe=JFactory::getApplication();
+		$response = array();
+		$query = JRequest::getVar('query', '');
+		$filename = JRequest::getVar('filename', 'export');
+		if (isset($query) && $query!='') {
+			$SQLmodel=$this->getModel('sql');
+			$Exportmodel=$this->getModel('export');
+			if($rows=$SQLmodel->getItems()){
+				$response['status'] = true;
+				$response['msg'] = 'Exported Sucessfully';
+				$exportedFileName=$Exportmodel->saveAsCSV($rows,$filename);
+				$response['download_url'] = JURI::base().'components'.DS.'com_jmm'.DS.'exported'.DS.$exportedFileName;
+			}else{
+				$response['status'] = false;
+				$response['msg'] = 'Erro Getting Data From Query';
+			}
+		}
+		echo json_encode($response);
+		$mainframe->close();
+	}
+
+	
+
+}
